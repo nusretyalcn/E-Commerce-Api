d9c237d36d9348774d274d462b263dda27a5f117
Nusret Yalçın <yalcnusret@gmail.com>
2024-02-19 22:45:12 +0300

    Entities Added

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public int Id { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public int CityId { get; set; }
+        public int DistrictId { get; set; }
+        public int RegionId { get; set; }
+        public int CountryId { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
