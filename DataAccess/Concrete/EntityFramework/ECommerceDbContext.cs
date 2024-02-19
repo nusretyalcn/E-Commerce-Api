583afbf98799f7ce7c44fafa2b337e7293817e6f
Nusret Yalçın <yalcnusret@gmail.com>
2024-02-19 23:24:04 +0300

    DataAccess Added

---
+++ DataAccess/Concrete/EntityFramework/ECommerceDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ECommerceDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-ER7LODGG\SQLEXPRESS01;
+                                          Database=E-CommerceDb;
+                                          Trusted_Connection=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
